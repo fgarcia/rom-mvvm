2eb9cb07be9cbf12eec83298fc23c593aca546fc
fgarcia <fgarcia@zenbits.io>
2015-01-12 12:48:53 +0100

    multiple models from same entity

--- demo.rb
+++ demo.rb
@@ -10,34 +10,45 @@ class UserEntity
   attribute :username, String
   attribute :email, String
   attribute :last_login, Time
+  attribute :admin, Boolean
 end
 
-setup = ROM.setup(memory: 'memory://playground') 
+# class UserRowModel
+#   include Virtus.model
+# end
+
+setup = ROM.setup(memory: 'memory://users') 
 
 setup.schema do
-  base_relation(:users) do
-    repository :memory
-  end
+  base_relation(:row) { repository :memory }
+  base_relation(:edit) { repository :memory }
 end
 
 setup.mappers do
-  define(:users) do
-    model name: 'UserSummaryModel'
-
+  define(:row) do
+    model name: 'UserRowModel'
+    #model UserRowModel
     attribute :name, :from => :username
+    attribute :email
+  end
+
+  define(:edit) do
+    model name: 'UserEditModel'
+    attribute :username
     attribute :email
   end
 end
 
 rom = setup.finalize
+# UserRowModel.send(:include, Equalizer.new(:name, :email))
 
-entity = UserEntity.new username: 'Jane', email:'jane@example.com'
-rom.schema.users << entity.to_h
+entity = UserEntity.new username: 'Jane', email: 'jane@example.com'
 
-UserSummaryModel.send(:include, Equalizer.new(:name, :email))
+rom.schema.row << entity.to_h
+user_model_row = rom.read(:row).to_a.first
 
-data = rom.read(:users).to_a.first
-# BUG_1 entity = UserEntity.new data
-require 'pry'; binding.pry
-ap data
+rom.schema.edit << entity.to_h
+user_model_edit = rom.read(:edit).to_a.first
+
+
 
